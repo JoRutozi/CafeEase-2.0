98eba66d03927e043e004d9db8d5332a43eee893
JoRutozi <131643571+JoRutozi@users.noreply.github.com>
2024-11-18 11:31:54 +0300

    Add files via upload

---
+++ staff/partials/login_check.php
@@ -0,0 +1,6 @@
+<?php
+if(!isset($_SESSION['user'])){
+    $_SESSION['no-login'] = "<div class='error'>Please Login to access Admin panel</div>";
+    header('location:login_staff.php');
+} 
+?>
---
+++ staff/partials/menu.php
@@ -0,0 +1,29 @@
+<?php include('../Config/constant.php'); ?>
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Staff Dashboard</title>
+    <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="../css/staff.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <!-- Menu section starts -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="profile.php">Profile</a></li>
+                <li><a href="category.php">Category</a></li>
+                <li><a href="food.php">Food</a></li>
+                <li><a href="order.php">Order</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+
+    </div>
+    <!-- Menu ends here -->
