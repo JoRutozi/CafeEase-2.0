5eceef89d9684b83e959424c1129003202c5664e
JoRutozi <131643571+JoRutozi@users.noreply.github.com>
2024-11-18 11:20:18 +0300

    Add files via upload

---
+++ customer/thank_you.php
@@ -0,0 +1,9 @@
+<?php
+include('../Config/constant.php');
+include('partial/menu.php');
+?>
+    <h1>Thank you for your payment!</h1>
+    <p>Your payment has been successfully processed.</p>
+    <!-- Additional details or redirection links as needed -->
+   <?php  include('partial/footer.php');
+    ?>
