814f6b2d71cd7ff1dfbc919d30aae0c98eed786e
JoRutozi <131643571+JoRutozi@users.noreply.github.com>
2024-11-18 11:13:17 +0300

    Add files in partials

---
+++ admin/partials/menu.php
@@ -0,0 +1,33 @@
+<?php include('../Config/constant.php'); ?>
+<?php 
+include('login_check.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <!-- Menu section starts -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="admin.php">Admin</a></li>
+                <li><a href="staff.php">Staff</a></li>
+                <li><a href="customer.php">Customer</a></li>
+                <li><a href="category.php">Category</a></li>
+                <li><a href="food.php">Food</a></li>
+                <li><a href="order.php">Order</a></li>
+                <li><a href="eWallet_admin.php">eWallet</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+
+    </div>
+    <!-- Menu ends here -->
